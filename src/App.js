93f92d0fee96419b41c499ba06a319d3bb0c0c4a
Priyanshu Singh <priyanshusinghverma1809@gmail.com>
2021-06-06 12:18:35 +0530

    Improved File Structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Article from "./components/Article/Article";
+import Brand from "./components/Brand/Brand";
+import Article from "./components/Article/Article";
+import Article from "./components/Article/Article";
 
 const App = () => {
   return <div>GPT_#</div>;
