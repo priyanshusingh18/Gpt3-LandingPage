f9e0f2613e1258fcde86a1f924c2a59869e56fbb
Priyanshu Singh <priyanshusinghverma1809@gmail.com>
2021-06-07 17:13:34 +0530

    brands html structure

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,7 +1,31 @@
 import React from "react";
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
 import "./brand.css";
+
 const Brand = () => {
-  return <div></div>;
+  return (
+    <div className="section__padding gpt3__brand">
+      <div className="">
+        <img src={google} alt="google" />
+      </div>
+      <div className="">
+        <img src={slack} alt="slack" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
