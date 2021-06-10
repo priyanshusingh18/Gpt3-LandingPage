338cb2f5d8a72ba23c63f8ca2179a4f6ed314cb0
Priyanshu Singh <priyanshusinghverma1809@gmail.com>
2021-06-10 13:43:52 +0530

    Footer Html bug fixed

--- src/components/containers/footer/Footer.js
+++ src/components/containers/footer/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <h1 className="gradient__text">
           Do you want to step in to the future before others
         </h1>
-        <div className="gpt3__footer-btn">
-          <p>Request Early Access</p>
-        </div>
+      </div>{" "}
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
       </div>
       <div className="gpt3__footer-links">
         <div className="gpt3__footer-links_logo">
